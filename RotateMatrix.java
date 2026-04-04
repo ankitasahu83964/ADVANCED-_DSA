940ae83a2f037be93fe8b43771ff6f7eba354be7
Ankita Sahu <ankitasahu83964@gmail.com>
2026-04-04 11:04:49 +0530

    Added Rotate Matrix java file

---
+++ RotateMatrix.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class RotateMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // square matrix (n x n)
+        int[][] arr = new int[n][n];
+
+        // Input
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        // Step 1: Transpose
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for(int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+
+            while(left < right) {
+                int temp = arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        // Output
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
