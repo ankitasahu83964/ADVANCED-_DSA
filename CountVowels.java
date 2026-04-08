2bef33f07be68c9673744f971259d0ccd78b261a
Ankita Sahu <ankitasahu83964@gmail.com>
2026-04-08 13:10:57 +0530

    Added CountVowels java program

---
+++ CountVowels.java
@@ -0,0 +1,21 @@
+public class CountVowels {
+    public static void main(String[] args) {
+        String str = "hello world";
+        int vowels = 0, consonants = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if(Character.isLetter(ch)) {
+                if("aeiouAEIOU".indexOf(ch) != -1) {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
