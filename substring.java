41d7b8420f9d5a1023fcfa73557ce96ef852462c
Ankita Sahu <ankitasahu83964@gmail.com>
2026-04-09 11:37:40 +0530

    Added substring.java program

---
+++ substring.java
@@ -0,0 +1,11 @@
+public class substring {
+        public static void main(String[] args) {
+            String str1="abc";
+            for(int i=0;i<str1.length();i++){
+                for(int j=i+1;j<=str1.length();j++){
+                        System.out.println(str1.substring(i,j));
+                }
+             }
+        }
+
+}
