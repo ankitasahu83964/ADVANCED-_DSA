911398cc178f06de542e6da7ef578cc4c1e87699
Ankita Sahu <ankitasahu83964@gmail.com>
2026-05-08 16:36:13 +0530

    Added BinarySearchRecursion.java Program

---
+++ BinarySearchRecursion.java
@@ -0,0 +1,25 @@
+public class BinarySearchRecursion {
+
+    static int binarySearch(int arr[], int left, int right, int key) {
+        if (left > right) return -1;  // base condition
+
+        int mid = (left + right) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        else if (key < arr[mid])
+            return binarySearch(arr, left, mid - 1, key);
+        else
+            return binarySearch(arr, mid + 1, right, key);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 6, 8, 10};
+        int result = binarySearch(arr, 0, arr.length - 1, 6);
+
+        if (result == -1)
+            System.out.println("Element not found");
+        else
+            System.out.println("Element found at index: " + result);
+    }
+}
