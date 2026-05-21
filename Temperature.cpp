b43b4c9ba38863fe880594c7c328beaf7abb8787
Ankita Sahu <ankitasahu83964@gmail.com>
2026-05-21 10:09:55 +0530

    Added Temperature.cpp program

---
+++ Temperature.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+
+        int n = temperatures.size();
+
+        vector<int> ans(n, 0);
+
+        stack<int> st; // stores indices
+
+        for (int i = 0; i < n; i++) {
+
+            // Current temperature greater than previous
+            while (!st.empty() &&
+                   temperatures[i] > temperatures[st.top()]) {
+
+                int idx = st.top();
+                st.pop();
+
+                ans[idx] = i - idx;
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    vector<int> temp = {73,74,75,71,69,72,76,73};
+
+    vector<int> ans = obj.dailyTemperatures(temp);
+
+    for (int x : ans) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
