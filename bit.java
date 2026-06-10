619b3b6ab7e7e94694ae911a50c59407f9c9c6f4
Ankita Sahu <ankitasahu83964@gmail.com>
2026-06-10 14:58:01 +0530

    Added bit.java program

---
+++ bit.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class bit{
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        int n=21;
+        int mask=(1<<2);
+        System.out.println(mask);
+    }    
+}
