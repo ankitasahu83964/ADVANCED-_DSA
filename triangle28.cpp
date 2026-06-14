3b64f37126b1a020660808393a2c2d88bcb3e451
Ankita sahu <ankitasahu83964@gmail.com>
2026-06-14 12:44:26 +0530

    Create triangle28.cpp

---
+++ triangle28.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+    int a[4][4];
+    int num = 1;
+
+    for(int i = 0; i < n; i++) {
+        if(i % 2 == 0) {
+            for(int j = 0; j < n; j++)
+                a[i][j] = num++;
+        } else {
+            for(int j = n-1; j >= 0; j--)
+                a[i][j] = num++;
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++)
+            cout << a[i][j] << " ";
+        cout << endl;
+    }
+}
