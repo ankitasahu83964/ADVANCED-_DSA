7a0162a8a98936e2b97d3080ba12c0feb277d51f
Ankita sahu <ankitasahu83964@gmail.com>
2026-06-19 12:27:25 +0530

    Implement scoreOfParentheses function

---
+++ leetcode16.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+
+        stack<int> st;
+        st.push(0);
+
+        for(char ch : s) {
+
+            if(ch == '(') {
+                st.push(0);
+            }
+            else {
+
+                int val = st.top();
+                st.pop();
+
+                int score = max(2 * val, 1);
+
+                st.top() += score;
+            }
+        }
+
+        return st.top();
+    }
+};
